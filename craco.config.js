d68fcb0c7d37beeef81bb6c8351c3a158a8ca2ef
Silent-code1 <64768412+Silent-code1@users.noreply.github.com>
2023-02-13 13:16:45 +0100

    Added craco

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+// craco.config.js
+
+const { CracoAliasPlugin } = require("react-app-alias");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAliasPlugin,
+      options: {},
+    },
+  ],
+};
